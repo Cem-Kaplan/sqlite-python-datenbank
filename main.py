2428c5369bea524d73c9c0123d63411fd1c2ae6a
Cem-Kaplan <cemk46908@gmail.com>
2025-06-08 08:53:30 +0200

    TKinter UI hinzugefügt

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import sqlite3
+import sqlite3, tkinter
+
+tk = tkinter.Tk()
+tk.title("Datenbank mit sqlite")
 
 connection = sqlite3.connect("produkte.db")
 cursor = connection.cursor()
@@ -9,11 +12,20 @@ connection.commit()
 def add_new_product(name, price):
     cursor.execute("INSERT INTO produkte (name, price) VALUES (?, ?)", (name, price))
 
-def show_products():
+def show_products_in_cmd():
     cursor.execute("SELECT * FROM produkte")
     for eintrag in cursor:
         print(eintrag)
 
+def show_products_in_ui():
+    cursor.execute("SELECT * FROM produkte")
+    for eintrag in cursor:
+        table = tkinter.Label(text=eintrag, width=100)
+        table.pack()
+
 add_new_product("PC", 200.00)
 add_new_product("Bread", 5.99)
-show_products()
+show_products_in_cmd()
+show_products_in_ui()
+
+tk.mainloop()
